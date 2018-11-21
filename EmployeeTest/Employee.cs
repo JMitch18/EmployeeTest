fd3949cff546dc617499a8c5321f462547450593
JMITCH18 <ronmitchell1@yahoo.com>
2018-11-20 20:09:21 -0600

    Employee montly salary

---
+++ EmployeeTest/Employee.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeTest
+{
+    class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        private decimal monthlySalary;
+
+
+        public Employee(string firstName, string lastName, decimal monthlySalary )
+        {
+
+            FirstName = firstName;
+            LastName = lastName;
+            this.monthlySalary = monthlySalary;
+
+        }
+
+        public decimal MonthlySalary
+        {
+            get
+            {
+                return monthlySalary;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    monthlySalary = value;
+                }
+            }
+        }
+    }
+}
---
+++ EmployeeTest/EmployeeTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeTest
+{
+    class EmployeeTest
+    {
+        static void Main(string[] args)
+        {
+
+            decimal yearlySalary = 0;
+
+            Employee employee1 = new Employee("Jaron", "Mitchell", 3000);
+            Employee employee2 = new Employee("Monica", "Mitchell", 5000);
+
+
+            Console.WriteLine("Employee 1 monthly salary after raise " + ( (employee1.MonthlySalary / 10) + employee1.MonthlySalary) * 12);
+            Console.Write("Employee 2 monthly salary after raise " + ( (employee2.MonthlySalary / 10) + employee2.MonthlySalary) * 12);
+            Console.ReadKey();
+
+        }
+    }
+}
